c7a8b3ce70a42a7d2b6d1be3daee46a7fcc97369
Damir Rysaev <freepad@ya.ru>
2024-05-06 10:34:40 +0300

    add config

---
+++ framework/config/configDummyjson.js
@@ -0,0 +1,8 @@
+import 'dotenv/config'
+
+export default Object.freeze({
+  baseURL:
+    process.env.TEST_DUMMYJSON_BASE_API_URL ?? 'TEST_DUMMYJSON_BASE_API_URL',
+  username: process.env.TEST_DUMMYJSON_USERNAME,
+  password: process.env.TEST_DUMMYJSON_PASSWORD,
+})
