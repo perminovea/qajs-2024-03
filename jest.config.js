1a7459f81a760bc45d2b98f186b2406c51d7cd96
Damir Rysaev <freepad@ya.ru>
2024-05-06 11:08:41 +0300

    got example

--- jest.config.js
+++ jest.config.js
@@ -176,11 +176,7 @@ const config = {
   // A map from regular expressions to paths to transformers
   // transform: undefined,
 
-  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "\\\\node_modules\\\\",
-  //   "\\.pnp\\.[^\\\\]+$"
-  // ],
+  transformIgnorePatterns: ['/node_modules/(?!got)/'],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
--- tests/auth.test.js
+++ tests/auth.test.js
@@ -1,17 +1,29 @@
 import { config as _config } from '../framework'
-import supertest from 'supertest'
+import got from 'got'
 
 const config = _config.dummyjson
 
+/**
+ * Чтобы got работал вместе с jest
+ * нужно добавить в jest.config.js
+ *   transformIgnorePatterns: [
+ *     "/node_modules/(?!got)/"
+ *   ]
+ * DOC: https://jestjs.io/docs/configuration#transformignorepatterns-arraystring
+ */
+
 describe('Auth', () => {
   it('Success login', async () => {
-    const response = await supertest(config.baseURL).post('/auth/login').send({
-      username: config.username,
-      password: config.password,
-      expiresInMins: 30,
+    const response = await got.post(`${config.baseURL}/auth/login`, {
+      json: {
+        username: config.username,
+        password: config.password,
+        expiresInMins: 30,
+      },
+      responseType: 'json',
     })
 
-    expect(response.status).toEqual(200)
+    expect(response.statusCode).toEqual(200)
     expect(response.body.username).toBe(config.username)
     expect(response.body.token).toBeTruthy()
   })
