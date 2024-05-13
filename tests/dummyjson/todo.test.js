edd0f2c1dca878db22c74b946c230024000183ee
Damir Rysaev <freepad@ya.ru>
2024-05-13 11:23:25 +0300

    upd todo.test.js

--- tests/dummyjson/todo.test.js
+++ tests/dummyjson/todo.test.js
@@ -1,4 +1,27 @@
+import Ajv from 'ajv'
 import { TodoSchema, TodoService } from '../../framework'
+
+const ajv = new Ajv()
+
+const todoSchema = {
+  type: 'object',
+  properties: {
+    id: {
+      type: 'number',
+    },
+    completed: {
+      type: 'boolean'
+    },
+    todo: {
+      type: 'string'
+    },
+    userId: {
+      type: 'number'
+    }
+  },
+  required: ['id', 'completed', 'todo', 'userId'],
+  additionalProperties: false
+}
 
 describe('Todo', () => {
   it('Should return a todo', async () => {
@@ -6,14 +29,14 @@ describe('Todo', () => {
     expect(response.status).toBe(200)
     expect(response.data).toStrictEqual({
       id: 1,
-      completed: false,
-      todo: 'Do something nice for someone you care about',
-      userId: 152,
+      completed: true,
+      todo: 'Do something nice for someone I care about',
+      userId: 26,
     })
   })
 
   it('Should return 404 if todo not exits', async () => {
-    const response = await TodoService.get(10_000)
+    const response = await TodoService.get(10_000) // 10000
     expect(response.status).toBe(404)
     expect(response.data).toStrictEqual({
       message: "Todo with id '10000' not found",
@@ -21,26 +44,22 @@ describe('Todo', () => {
   })
 
   it('Should return a random todo', async () => {
-    const response = await TodoService.getRandom()
-    expect(response.status).toBe(200)
-    expect(response.data).toMatchSchema(TodoSchema)
+    const response1 = await TodoService.getRandom()
+
+    const validate1 = ajv.validate(todoSchema, response1.data)
+    expect(response1.status).toBe(200)
+    expect(validate1).toBe(true)
+
+    const response2 = await TodoService.getRandom()
+
+    const validate2 = ajv.validate(todoSchema, response2.data)
+    expect(response2.status).toBe(200)
+    expect(validate2).toBe(true)
+
+    expect(response1.data).not.toStrictEqual(response2.data)
   })
 
-  it('Should return all todo by user id', async () => {
-    const response = await TodoService.getAllByUserId(1)
-    expect(response.status).toBe(200)
-    expect(response.data).toMatchObject({
-      limit: 2,
-      skip: 0,
-      total: 2,
-      todos: expect.any(Array),
-    })
-    expect(response.data.todos.length).toBe(2)
-    for (const todo of response.data.todos) {
-      expect(todo).toMatchSchema(TodoSchema)
-    }
-  })
-
+  it.todo('Should return all todo by user id')
   it.todo('Should correct add new todo')
   it.todo('Should correct update todo')
   it.todo('Should delete todo')
