80b96a2c0d5d83332566ec33b86e2286c0bd07fb
Damir Rysaev <freepad@ya.ru>
2024-07-01 17:45:20 +0300

    classes in js

--- framework/services/AuthService.js
+++ framework/services/AuthService.js
@@ -1,50 +1,50 @@
 import config from '../config/configBookstore'
 import { cached } from '../utils/cache'
 
-const generateToken = async ({ userName, password }) => {
-  const response = await fetch(`${config.baseURL}/Account/v1/GenerateToken`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ userName, password }),
-  })
+class AuthService {
+  static async generateToken  ({ userName, password }) {
+    const response = await fetch(`${config.baseURL}/Account/v1/GenerateToken`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ userName, password }),
+    })
 
-  return {
-    headers: response.headers,
-    status: response.status,
-    data: await response.json(),
+    return {
+      headers: response.headers,
+      status: response.status,
+      data: await response.json(),
+    }
+  }
+
+  static async authorized ({ userName, password }) {
+    // this....
+    const response = await fetch(`${config.baseURL}/Account/v1/Authorized`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ userName, password }),
+    })
+
+    return {
+      headers: response.headers,
+      status: response.status,
+      data: await response.json(),
+    }
+  }
+
+  static generateTokenCached () {
+    return cached(AuthService.generateToken)
+  }
+
+  static getTokenFromCache = async ({ userName, password }) => {
+    const response = await AuthService.generateTokenCached({
+      userName,
+      password,
+    })
+    if (typeof response.data.token !== 'string') {
+      throw new Error('No token in response')
+    }
+    return response.data.token
   }
 }
 
-const authorized = async ({ userName, password }) => {
-  const response = await fetch(`${config.baseURL}/Account/v1/Authorized`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ userName, password }),
-  })
-
-  return {
-    headers: response.headers,
-    status: response.status,
-    data: await response.json(),
-  }
-}
-
-const generateTokenCached = cached(generateToken)
-
-const getTokenFromCache = async ({ userName, password }) => {
-  const response = await generateTokenCached({
-    userName,
-    password,
-  })
-  if (typeof response.data.token !== 'string') {
-    throw new Error('No token in response')
-  }
-  return response.data.token
-}
-
-export default {
-  generateToken,
-  generateTokenCached,
-  getTokenFromCache,
-  authorized,
-}
+export { AuthService }
---
+++ src/classes.mjs
@@ -0,0 +1,100 @@
+import { setTimeout } from 'node:timers/promises'
+
+const locators = {}
+
+class BasicPage {
+  constructor({ page }) {
+    // this = {}
+
+    this.page = page
+
+    this.headerButton = 'мой локатор'
+
+    BasicPage.common = ''
+
+    // return this
+  }
+
+  // this.visit = |
+  async visit () {
+    await this.page.goto(this.url)
+    // 1
+    console.log(`BasicPage.goto Open page: ` + this.url)
+  }
+}
+
+export class AuthPage extends BasicPage {
+  url = '/login'
+}
+export class ArticlePage extends BasicPage {
+  #id
+
+  constructor({ page, id }) {
+    super({ page })
+
+    this.#id = id;
+  }
+
+  get url () {
+    return `/article/${this.#id}`
+  }
+}
+export class PostPage extends BasicPage {
+  constructor({ page, id }) {
+    super({ page })
+
+    this.url = `/article/${id}`
+  }
+}
+
+const page = {
+  async goto (url) {
+    await setTimeout(10_000)
+    console.log('page.goto Open page: ', url)
+  }
+}
+const authPage1 = new AuthPage({ page }) // .constructor // создали this
+const authPage2 = new AuthPage({ page }) // .constructor // создали this
+const articlePage = new ArticlePage({ page, id: 101 })
+
+await authPage1.visit()
+// authPage1.visit() -> this.visit()
+
+await articlePage.visit()
+
+(new AuthPage({ page }) /* this -> */).visit() // this.visit
+
+class UserFixture {
+  state = {}
+
+  setUsername (username, uniq = false) {
+    this.state.username = username
+
+    if (uniq) {
+      this.state.username = username + Math.random() // UUID()
+    }
+
+    return this;
+  }
+
+  setPassword (password) {
+    this.state.password = password
+
+    return this;
+  }
+
+  build () {
+    return {}
+  }
+}
+
+
+const userFixture = new UserFixture()
+
+userFixture
+  .setUsername('123123')
+  .setPassword('123456')
+
+const user1 = userFixture.build()
+
+const user2 = userFixture.setPassword('new password').build()
